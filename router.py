ad83afc4b0826d8d6483b29eceaa8b964e6af14d
miazigoo <kasper6024@gmail.com>
2025-03-28 19:02:31 +0300

    Кошелек сохраняется в БД, Возвращается bandwidth, balance, energy

--- router.py
+++ router.py
@@ -1,8 +1,13 @@
-from fastapi import APIRouter
+from decimal import Decimal
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from tronpy import Tron
 
 from database import engine, new_session
-from models import Base
-
+from models import Base, WalletRequest
+from schemas import AddressInput, InfoOutput
 
 router = APIRouter()
 
@@ -16,3 +21,30 @@ async def get_db():
     finally:
         db.close()
         print("Выключение DB")
+
+
+@router.post("/wallet_info", response_model=InfoOutput)
+async def wallet_info(
+        task_in: AddressInput, db: Session = Depends(get_db)
+):
+    """
+    Делаем запись запроса в БД.
+    Записываются id, адрес кошелька и дата запроса.
+    Возвращаем информацию с кошелька:
+    bandwidth, balance, energy
+    """
+    address = task_in.address
+    if not Tron.is_address(address):
+        raise HTTPException(status_code=400, detail="Не верный адрес кошелька")
+    task = WalletRequest(address=address)
+    db.add(task)
+    db.commit()
+    tron = Tron(network='nile')
+    bandwidth = tron.get_bandwidth(address)
+    account = tron.get_account(address)
+    info = {
+        'bandwidth': bandwidth,
+        'balance': Decimal(account['balance']) / 1_000_000,
+        'energy': Decimal(account['net_window_size']) / 1_000_000
+    }
+    return InfoOutput(**info)
--- schemas.py
+++ schemas.py
@@ -3,3 +3,8 @@ from pydantic import BaseModel
 
 class AddressInput(BaseModel):
     address: str
+
+class InfoOutput(BaseModel):
+    bandwidth: int
+    balance: float
+    energy: float
